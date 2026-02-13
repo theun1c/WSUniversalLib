dfbae6cfe3c818e3be90651a423b4387b2efb061
theun1c <belovdima1122@yandex.ru>
2026-02-13 21:15:01 +0300

    add const

--- WSUniversalLib/Class1.cs
+++ WSUniversalLib/Class1.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace WSUniversalLib
 {
-    public class Class1
+    public class Calculation
     {
+        // coeff
+        private float _1stTypeCoeff = 1.1F;
+        private float _2ndTypeCoeff = 2.5F;
+        private float _3rdTypeCoeff = 8.43F;
+
+        // defect percent
+        private float _1stTypeDefect = 0.3F;
+        private float _2ndTypeDefect = 0.12F;
+
+        public int GetQuantityForProduct(int productType, int materialType, int count, float width, float length)
+        {
+
+        }
     }
 }
